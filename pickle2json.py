92c095ed0b1160db10c559f5338c9d47f3732f54
Matt Long <matt@burythehammer.com>
2017-01-05 13:09:01 +0000

    Modifications to output json

--- pickle2json.py
+++ pickle2json.py
@@ -1,19 +1,31 @@
-import json as JSON
 import pickle
 import jsonpickle
+import json as JSON
 
 INPUT_FILE_PATH = "foosdb.pickle"
 OUTPUT_FILE_PATH = "foosdb.json"
 
 def read_results(input_path):
-    f = open(input_path, 'rw')
-    return pickle.loads(f.read())
+    pickle_file = open(input_path, 'r')
+    something = pickle_file.read
+    print type(something)
+    return pickle.loads(pickle_file.read())
 
-def write_results(results, output_path):
+def write_results_to_json(results, output_path):
     serialized = jsonpickle.encode(results)
+    json_file = open(output_path, 'w')
+    json_file.write(serialized)
 
-    f = open(output_path, 'w')
-    f.write(serialized)
+def pickle2json():
+    results = read_results(INPUT_FILE_PATH)
+    write_results_to_json(results, OUTPUT_FILE_PATH)
 
-results = read_results(INPUT_FILE_PATH)
-write_results(results, OUTPUT_FILE_PATH)
+
+def json2python():
+    output_file = open(OUTPUT_FILE_PATH, 'r')
+    results = JSON.loads(output_file.read)
+    print results 
+
+
+pickle2json()
+json2python()
